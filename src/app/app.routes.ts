1c3f5390bc8e8998880eff6a1fc2e0d561c04c0c
Jayden Lewis <jaydent.lewis@gmail.com>
2026-04-08 15:30:30 -0400

    Added mission list component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Missionlist } from './missionlist/missionlist';
+import { Missiondetails } from './missiondetails/missiondetails';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', redirectTo: 'missions', pathMatch: 'full' },
+    { path: 'missions', component: Missionlist },
+    { path: 'mission/:id', component: Missiondetails }
+];
--- src/app/missionlist/missionlist.ts
+++ src/app/missionlist/missionlist.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { Spacexapi } from '../network/spacexapi';
+import { Mission } from '../models/mission';
 
 @Component({
   selector: 'app-missionlist',
@@ -6,4 +11,17 @@ import { Component } from '@angular/core';
   templateUrl: './missionlist.html',
   styleUrl: './missionlist.css',
 })
-export class Missionlist {}
+export class Missionlist implements OnInit {
+
+  missions: Mission[] = [];
+
+  constructor(private spacexApi: Spacexapi) {}
+
+  ngOnInit(): void {
+
+    this.spacexApi.getMissions().subscribe((data) => {
+
+      this.missions = data;
+    });
+  }
+}
