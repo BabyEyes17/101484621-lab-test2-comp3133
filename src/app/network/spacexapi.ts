83b1888f368c42386ec732445cc735abd198915b
Jayden Lewis <jaydent.lewis@gmail.com>
2026-04-08 15:18:47 -0400

    Configured API service requests

--- src/app/network/spacexapi.ts
+++ src/app/network/spacexapi.ts
@@ -1,6 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Mission } from '../models/mission';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Spacexapi {}
+
+export class Spacexapi {
+
+  private apiUrl = 'https://api.spacexdata.com/v3/launches';
+
+  constructor(private http: HttpClient) {}
+
+  // Get all Missions
+  get missions(): Observable<Mission[]> {
+
+    return this.http.get<Mission[]>(this.apiUrl)
+  }
+
+  // Get Mission by flight number
+  getMission(flightNumber: number): Observable<Mission> {
+
+    return this.http.get<Mission>(`${this.apiUrl}/${flightNumber}`);
+  }
+
+  // Get Missions by year
+  getMissionsByYear(year: number): Observable<Mission[]> {
+
+    return this.http.get<Mission[]>(`${this.apiUrl}?launch_year=${year}`);
+  }
+}
