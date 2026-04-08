3e4426fcd07d3362f5752d1466b381b42304636a
Jayden Lewis <jaydent.lewis@gmail.com>
2026-04-08 19:04:44 -0400

    Added filter methods, configured input field for best UX, and completed styling on filter container

--- src/app/missionfilter/missionfilter.ts
+++ src/app/missionfilter/missionfilter.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon'; 
 
 @Component({
   selector: 'app-missionfilter',
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
   templateUrl: './missionfilter.html',
   styleUrl: './missionfilter.css',
 })
-export class Missionfilter {}
+
+export class Missionfilter {
+
+  selectedYear: string = '';
+
+  @Output() yearSelected = new EventEmitter<string>();
+  
+  onFilter(value: any): void {
+    const yearString = value ? value.toString() : '';
+    this.yearSelected.emit(yearString);
+  }
+
+  onClear(): void {
+    
+    this.selectedYear = '';
+    this.yearSelected.emit('');
+  }
+}
