68e77502744716d56b64f13c0cdef68dce6fc4ff
Jayden Lewis <jaydent.lewis@gmail.com>
2026-04-08 19:00:34 -0400

    Added more fields to Mission model

--- src/app/models/mission.ts
+++ src/app/models/mission.ts
@@ -1,21 +1,32 @@
 export interface Mission {
-
-    flight_number: number;
-    mission_name: string;
-    launch_year: string;
-    details: string
-    
-    rocket: {
-        
-        rocket_name: string;
-        rocket_type: string
+  flight_number: number;
+  mission_name: string;
+  launch_year: string;
+  launch_date_local: string;
+  launch_success: boolean;
+  details: string;
+  rocket: {
+    rocket_name: string;
+    rocket_type: string;
+    second_stage: {
+      payloads: Array<{
+        payload_id: string;
+        payload_type: string;
+        orbit: string;
+      }>;
     };
-
-    links: {
-
-        mission_patch_small: string;
-        article_link: string;
-        wikipedia: string;
-        video_link: string
-    };
+  };
+  launch_site: {
+    site_name: string;
+    site_name_long: string;
+  };
+  launch_failure_details: {
+    reason: string;
+  };
+  links: {
+    mission_patch_small: string;
+    article_link: string;
+    wikipedia: string;
+    video_link: string;
+  };
 }
